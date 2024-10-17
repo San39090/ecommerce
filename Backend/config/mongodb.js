0d24e747f51635615b3fa6760f31cba968584d2c
San39090 <121235533+San39090@users.noreply.github.com>
2024-10-17 16:18:01 +0530

    Update mongodb.js

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async()=>{
     mongoose.connection.on('connected',()=>{
         console.log("DB connected");
     })
-    await mongoose.connect(`${process.env.MONGODB_URI}/?retryWrites=true&w=majority&appName=Cluster0`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/ecommerce`)
 }
 
 export default connectDB;
