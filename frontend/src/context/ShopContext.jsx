c342c422c26ff6a8b2948d8779be85bee0b0f33d
San39090 <121235533+San39090@users.noreply.github.com>
2024-10-17 17:06:56 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -8,7 +8,7 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props)=>{
     const currency = '$';
     const delivery_fee = 10;
-    const backendUrl = "https://ecommerce-backend-flame-delta.vercel.app"
+    const backendUrl = "";
     const [search,setSearch] = useState('');
     const [showSearch,setShowSearch] = useState(false);
     const [products,setProducts] = useState([]);
